96161c13e550de707de4b9f57861be6338fabeb9
Ankit Gupta <argankit@gmail.com>
2019-03-20 19:41:35 +0530

    tweaks

--- detect_face.py
+++ detect_face.py
@@ -7,7 +7,7 @@ import math
 face_cascade = cv2.CascadeClassifier("face_detection_models\haarcascade_frontalface_default.xml")
 eye_cascade = cv2.CascadeClassifier("face_detection_models\haarcascade_eye.xml")
 
-img = cv2.imread("standard_test_images/woman_darkhair.tif")
+img = cv2.imread("standard_test_images/lena_color_512.tif")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 sunglasses = cv2.imread("sunglasses.png")
